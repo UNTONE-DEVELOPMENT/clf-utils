baf3070d3c716d1fbbf8ec2eec2a419f2244f125
BuildTools <33783503+Hubza@users.noreply.github.com>
2021-12-29 20:19:17 +0000

    add method to load only meta of level

--- clfFormat.cs
+++ clfFormat.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
@@ -582,9 +583,8 @@ background = 0,128,229;247,237,217
 
         public static string[] readClfLine(string line)
         {
-            line = line.Replace(" = ", "%");
-            char split = '%';
-            string[] splitted = line.Split(split);
+            Debug.Log("splitting line " + line);
+            string[] splitted = line.Split(" = ");
             return splitted;
         }
 
@@ -674,6 +674,51 @@ background = " + backgroundString + @"
             }
 
             return clf;
+        }
+
+        public static clfFile loadLimited(string[] clf)
+        {
+            clfFile file = newClfFile();
+
+            string[] general = getSection("General", clf);
+            foreach (string x in general)
+            {
+                Debug.Log(x);
+                string[] line = readClfLine(x);
+                if (line[0] == "name")
+                {
+                    file.general.name = line[1];
+                }
+                if (line[0] == "description")
+                {
+                    file.general.description = line[1];
+                }
+                if (line[0] == "creator")
+                {
+                    file.general.creator = line[1];
+                }
+                if (line[0] == "tags")
+                {
+                    file.general.tags = line[1];
+                }
+            }
+
+
+            return file;
+        }
+        
+        public static clfFile loadLimitedFromFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                string[] first10Lines = File.ReadLines(path).Take(10).ToArray<string>();
+                return loadLimited(first10Lines);
+            }
+            else
+            {
+                Debug.LogError("Could not find CLF file for " + path);
+                return null;
+            }
         }
     }
 }
