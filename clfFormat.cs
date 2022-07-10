083133faf618dbd9658610af013b3cbb58416c1e
Hubza <33783503+Hubza@users.noreply.github.com>
2022-07-10 22:58:01 +0100

    fix

--- clfFormat.cs
+++ clfFormat.cs
@@ -584,7 +584,8 @@ background = 0,128,229;247,237,217
         public static string[] readClfLine(string line)
         {
             //Debug.Log("splitting line " + line);
-            string[] splitted = line.Split(" = ");
+            line = line.Replace(" = ", "=");
+            string[] splitted = line.Split('=');
             return splitted;
         }
 
